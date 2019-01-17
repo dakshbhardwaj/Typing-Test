a2c2526c52839e67d6aa9a714f5d873851aab5e8
dakshbhardwaj2@gmail.com <dakshbhardwaj2@gmail.com>
2019-01-18 03:27:58 +0530

    final-commit

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+import "./App.css";
+import textString from "./text";
+import swal from "sweetalert";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.startQuiz = this.startQuiz.bind(this);
+    this.handleTextArea = this.handleTextArea.bind(this);
+    this.renderDisplayText = this.renderDisplayText.bind(this);
+    this.endQuiz = this.endQuiz.bind(this);
+    this.state = {
+      seconds: 60,
+      count: 0,
+      text: textString.trim().split(" "),
+      textArea: "",
+      started: false,
+      score: null
+    };
+  }
+
+  startQuiz() {
+    this.setState({ started: true, score: null, seconds: 60, count: 0 });
+    this.interval = setInterval(() => {
+      if (this.state.seconds - 1 === 0) {
+        this.endQuiz();
+        clearInterval(this.interval);
+      }
+      this.setState({ seconds: this.state.seconds - 1 });
+    }, 1000);
+  }
+
+  handleTextArea(event) {
+    const count = event.target.value.replace(/\s+/g, " ").split(" ").length - 1;
+    this.setState({ textArea: event.target.value, count });
+  }
+
+  renderDisplayText() {
+    return this.state.text.map((word, index) => {
+      return (
+        <span
+          key={index}
+          className={index === this.state.count ? `highlight` : ""}
+        >{`${word} `}</span>
+      );
+    });
+  }
+
+  endQuiz() {
+    let score = 0;
+    const inputText = this.state.text;
+    const writtenText = this.state.textArea.trim().split(" ");
+    const noOfWrittenWords = writtenText.length;
+    for (let i = 0; i < noOfWrittenWords; i++) {
+      if (writtenText[i] === inputText[i]) {
+        score += 10;
+      } else {
+        score -= 5;
+      }
+    }
+    swal({
+      icon: "success",
+      title: "Time Is Up",
+      text: `Your Score Is ${score}\nWords Written ${noOfWrittenWords}\nWords Per Seconds ${(
+        noOfWrittenWords / 60
+      ).toFixed(2)}`,
+      closeOnClickOutside: false,
+      closeOnEsc: false
+    });
+    this.setState({
+      score,
+      count: noOfWrittenWords,
+      started: false,
+      textArea: "",
+      inputText: []
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark bg-dark justify-content-between">
+          <a className="navbar-brand white-text" href="/">
+            Typing Test
+          </a>
+          <div className="form-inline">
+            <h6 className="btn btn-danger">{`Time Left: ${
+              this.state.seconds
+            } Secs`}</h6>
+          </div>
+        </nav>
+        <div className="container">
+          {this.state.score != null && (
+            <div className="alert alert-info" role="alert">
+              {`Your Score Is ${this.state.score}\nWords Written ${
+                this.state.count
+              }\nWords Per Seconds ${(this.state.count / 60).toFixed(2)}`}
+            </div>
+          )}
+
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Type this text :</h5>
+                  <p className="card-text text-box">
+                    {this.renderDisplayText()}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {this.state.started ? (
+            <React.Fragment>
+              <div className="row">
+                <div className="col-md-6 offset-md-3">
+                  <textarea
+                    rows="6"
+                    className="input-box"
+                    onChange={this.handleTextArea}
+                    value={this.state.textArea}
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6 offset-md-3">
+                  <a href="#"  className="input-box btn btn-primary">
+                    End Test
+                  </a>
+                </div>
+              </div>
+            </React.Fragment>
+          ) : (
+            <div className="row">
+              <div className="col-md-6 offset-md-3">
+                <a
+                  href="#"
+                  onClick={() => {
+                    this.startQuiz();
+                  }}
+                  className="input-box btn btn-primary"
+                >
+                  Start Test
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
